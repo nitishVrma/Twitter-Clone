914c06f8955f8dd83747e3223b8e223db76cd69c
Nitish <ozeus459@gmail.com>
2021-09-30 12:12:55 +0530

    sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import Signup from "./components/signup/Signup";
 import Sidebar from "./components/sidebar";
 import { Explore } from "./Pages/Explore";
 import { Notification } from "./Pages/Notification";
@@ -12,36 +13,37 @@ import { Posts } from "./Pages/Posts";
 
 function App() {
   return (
-    <Grid container className="App">
-      <Grid item sm={2} md={3} align="center">
-        <Sidebar />
-      </Grid>
-      <Grid item sm={9} md={6}>
-        <Switch>
-          <Route path="/home">
-            <Posts />
-          </Route>
-          <Route path="/explore">
-            <Explore />
-          </Route>
-          <Route path="/notification">
-            <Notification />
-          </Route>
-          <Route path="/messages">
-            <Messages />
-          </Route>
-          <Route path="/bookmark">
-            <Bookmarks />
-          </Route>
-          <Route path="/list">
-            <Lists />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
-      </Grid>
-    </Grid>
+    <Signup/>
+    // <Grid container className="App">
+    //   <Grid item sm={2} md={3} align="center">
+    //     <Sidebar />
+    //   </Grid>
+    //   <Grid item sm={9} md={6}>
+    //     <Switch>
+    //       <Route path="/home">
+    //         <Posts />
+    //       </Route>
+    //       <Route path="/explore">
+    //         <Explore />
+    //       </Route>
+    //       <Route path="/notification">
+    //         <Notification />
+    //       </Route>
+    //       <Route path="/messages">
+    //         <Messages />
+    //       </Route>
+    //       <Route path="/bookmark">
+    //         <Bookmarks />
+    //       </Route>
+    //       <Route path="/list">
+    //         <Lists />
+    //       </Route>
+    //       <Route path="/profile">
+    //         <Profile />
+    //       </Route>
+    //     </Switch>
+    //   </Grid>
+    // </Grid>
   );
 }
 
